72be8d7e5367b9f965e17ae6f4e752af31d22e39
Anders <andetel@stud.ntnu.no>
2022-04-20 15:02:49 +0200

    Created new component for creating a list of ads

---
+++ src/components/AdListComponent.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="d-flex justify-content-center align-items-center">
+    <div class="d-flex flex-row justify-content-start align-items-center flex-wrap mx-auto w-100">
+
+      <AdComponent v-for="ad in ads" :key="ad" :title="ad.title" :image="ad.img" :place="ad.place" :price="ad.price" />
+
+    </div>
+  </div>
+</template>
+
+<script>
+import AdComponent from "@/components/AdComponent";
+
+export default {
+  name: "AdListComponent",
+  components: {
+    AdComponent
+  },
+  props: {
+    ads: {
+      type: Array,
+      required: true
+    }
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
