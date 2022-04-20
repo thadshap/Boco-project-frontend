f23005da610c66e9b1ef1ff1cdd3a6060ae834ef
Anders <andetel@stud.ntnu.no>
2022-04-20 15:03:26 +0200

    Created main page view

---
+++ src/views/MainPage.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="container">
+    <div class="d-flex justify-content-center search-box-container-style">
+      <div class="d-flex search-container">
+        <input type="search" placeholder="Søk" class="w-100">
+        <button class="btn btn-primary btn-style" type="button">
+          <i class="fa fa-search"></i>
+        </button>
+      </div>
+    </div>
+
+    <div>
+      <h3>Categories</h3>
+      <div class="d-flex flex-row justify-content-start align-items-center flex-wrap categories-card-container-style">
+
+        <CategoryComponent v-for="category in categories" :key="category" :title="category.title"/>
+
+      </div>
+    </div>
+  </div>
+
+  <div>
+    <h3>Newest items</h3>
+
+    <AdListComponent :ads="ads"/>
+
+  </div>
+</template>
+
+<script>
+import AdListComponent from "@/components/AdComponent";
+import CategoryComponent from "@/components/CategoryComponent";
+
+export default {
+  name: "MainPage",
+  components:{
+    AdListComponent,
+    CategoryComponent
+  },
+  data() {
+    return {
+      ads: [],
+      categories: []
+    }
+  }
+};
+</script>
+
+<style scoped>
+.search-box-container-style {
+  margin-bottom: 20px;
+}
+
+.btn-style {
+  border-top-left-radius: 0;
+  border-bottom-left-radius: 0;
+  width: 80px;
+}
+
+.categories-card-container-style {
+  padding: 10px;
+}
+
+@media (min-width: 992px) {
+  .search-container {
+    width: 50%;
+  }
+}
+
+@media (max-width: 991px) {
+  .search-container {
+    width: 80%;
+  }
+}
+</style>
