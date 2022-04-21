de15bdfa5b12a59e536b02962abf79e3bea06d7c
Brage <bragmin@hotmail.com>
2022-04-21 09:24:58 +0200

    Added a test for Ad component

---
+++ tests/unit/AdComponent.spec.js
@@ -0,0 +1,17 @@
+import { shallowMount } from "@vue/test-utils";
+import AdComponent from "@/components/AdComponent";
+
+describe("AdComponent.vue", () => {
+    it("renders props when passed", () => {
+        const title = "Motorsag";
+        const price = 1000;
+        const place = "Trondheim";
+        const image = "Dette er et bilde";
+        const wrapper = shallowMount(AdComponent, {
+            props: { title,price,place,image },
+        });
+        expect(wrapper.text()).toMatch(title);
+        expect(wrapper.text()).toMatch(place);
+        expect(wrapper.text()).toMatch(price.toString());
+    });
+});
