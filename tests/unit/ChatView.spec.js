bdc84d4d89fd696d95ce350e6241375514ef59b5
Brage <bragmin@hotmail.com>
2022-04-25 13:22:41 +0200

    fixed tests for chat

--- tests/unit/ChatView.spec.js
+++ tests/unit/ChatView.spec.js
@@ -1,25 +1,28 @@
 import { mount } from '@vue/test-utils'
 import Chat from "@/views/Chat";
 
+describe("Chatview", () => {
+
     test('Renders menu button', () => {
     const wrapper = mount(Chat)
-  
+
     const button = wrapper.get('[class="btn btn-primary menu-button"]')
-  
+
     expect(button.exists()).toBe(true)
   })
   test('Renders username', () => {
     const wrapper = mount(Chat)
-  
+
     const username = wrapper.get('[class="name"]')
     //TODO: Fix expected text when chat name shows
-    expect(username.text()).toBe("Text")
+    expect(username.text()).toEqual("Username")
   })
   test('Renders picture', () => {
     const wrapper = mount(Chat)
-  
+
     const picture = wrapper.get('[class="profile-picture"]')
     //TODO: Fix expected text when chat name shows
     expect(picture.exists()).toBe(true)
   })
-  
+})
+
