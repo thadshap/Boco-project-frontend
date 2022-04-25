91324b404d748127d428f4668379518882b48ebb
Brage <bragmin@hotmail.com>
2022-04-25 11:23:15 +0200

    removed non functional 'this.store' calls

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -16,7 +16,7 @@
       </button>
         <div class="collapse navbar-collapse" id="navcol-2">
           <ul class="navbar-nav ms-auto">
-            <li class="nav-item" v-if="this.$store.getters.loggedIn">
+            <li class="nav-item">
 
               <router-link to="/new_ad" class="nav-link active text-end">
                 <i class="fa fa-plus-square"></i>
@@ -24,7 +24,7 @@
               </router-link>
               
             </li>
-            <li class="nav-item" v-if="this.$store.getters.loggedIn">
+            <li class="nav-item">
               
               <router-link to="/" class="nav-link active text-end">
                 <i class="fa fa-envelope"></i>
@@ -32,7 +32,7 @@
               </router-link>
               
             </li>
-            <li class="nav-item" v-if="this.$store.getters.loggedIn">
+            <li class="nav-item">
               
               <router-link to="/my_profile" class="nav-link active text-end">
                 <i class="fa fa-user"></i>
@@ -41,7 +41,7 @@
               
             </li>
 
-            <li class="nav-item" v-if="!this.$store.getters.loggedIn">
+            <li class="nav-item">
 
               <router-link to="/login" class="nav-link active text-end d-flex flex-row align-items-center">
                 <span>Log in</span>
