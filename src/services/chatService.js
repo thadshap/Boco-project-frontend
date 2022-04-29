b93ee69c021a30176b875f5f336fb160a6fab968
leo <leonarss@stude.ntnu.no>
2022-04-29 13:37:10 +0200

    feat: can get old messages from DB

--- src/services/chatService.js
+++ src/services/chatService.js
@@ -1,20 +1,16 @@
 import axios from "axios";
 let url = 'http://localhost:'
-let port = "8443"
+let port = "8080"
 export default{
-    getMessagesByGroupId(){
+    getMessagesByGroupId(groupId){
         const options = {
             method: 'GET',
-            url: `${url}${port}/api/group/messages/1096`,
+            url: `${url}${port}/api/group/messages/${groupId}`,
             headers: {
               Authorization: localStorage.getItem("userId")
             }
           };
           
-          axios.request(options).then(function (response) {
-            console.log(response.data);
-          }).catch(function (error) {
-            console.error(error);
-          });
+          return axios.request(options)
     }
 }
