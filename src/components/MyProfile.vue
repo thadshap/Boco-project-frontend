dc336d9117e49d1daeba0a5649f775b4eb2e029a
Thadshajini Paramsothy <thadshap@stud.ntnu.no>
2022-05-03 12:33:56 +0200

    finished designing my profile page

--- src/components/MyProfile.vue
+++ src/components/MyProfile.vue
@@ -1,16 +1,19 @@
 <template>
-  <div class="d-flex flex-column container p-5 bg-info">
-    <div class="d-flex flex-row justify-content-center w-100">
+  <div class="d-flex flex-column container p-5 main-container">
+    <div class="d-flex flex-row justify-content-center w-100 p-3 user-info-wrapper">
       <div class="infoContainerDiv">
-        <div class="card infoCard">
-          <div class="card-body">
-            <div>
-              <h3>Min Profil</h3>
-            </div>
-            <div class="d-flex infoDiv"><img class="profilePic" src="@/assets/img/profilePicPlaceholder.png">
+        <!---------------------------------------------------------------------------------------------------------------------------------------------------------------------------->
+        <div class="infoCard">
+          <div class="container d-flex justify-content-center">
+            <div class="user-info">
+              <div class="image-container-wrapper d-flex justify-content-center align-items-center">
+              <div class="image-container image d-flex flex-column justify-content-center align-items-center">
+                <img class="profilePic" height="100" width="100" src="@/assets/img/profilePicPlaceholder.png">
+              </div>
+              </div>
               <div class="d-sm-flex flex-column justify-content-end">
                 <h1>Brage Minge</h1>
-                <h6 style="opacity: 0.70;"><br>bragemi@stud.ntnu.no<br><br></h6>
+                <div style="opacity: 0.80;">bragemi@stud.ntnu.no<br><br></div>
                 <div class="d-flex justify-content-between">
                   <p><strong>10.0</strong></p>
                   <p>3 vurderinger</p>
@@ -19,6 +22,8 @@
             </div>
           </div>
         </div>
+        <!---------------------------------------------------------------------------------------------------------------------------------------------------------------------------->
+
       </div>
     </div>
     <div class="d-flex flex-row flex-wrap optionContainer w-100 mt-5">
@@ -64,7 +69,7 @@
       </div>
       <div class="cardDiv">
         <div class="card h-100" @click="logout">
-          <div class="card-body cardBodyStyle d-flex align-items-center justify-content-center">
+          <div class="card-body cardBodyStyle d-flex align-items-center justify-content-center sign-out-container">
             <h4 class="card-title">Logg ut</h4>
           </div>
         </div>
@@ -135,15 +140,19 @@
   width: 100%;
   height: 100%;
   cursor: pointer;
+  background-color: rgb(70, 156, 175);
+  color: rgba(255, 255, 255, 0.9);
 }
 
 .cardBodyStyle:hover {
-  background-color: rgb(175, 182, 189);
+  background-color: #378A91FF;
   box-shadow: 0px 5px 5px 2px rgba(0,0,0,0.53);
 }
 
-.infoDiv {
-  gap: 1vw;
+.sign-out-container:hover {
+  background-color: #BD0303FF;
+  color: white;
+  box-shadow: 0px 5px 5px 2px rgba(0,0,0,0.53);
 }
 
 .containerDiv {
@@ -152,14 +161,36 @@
   min-height: 100vh;
 }
 
+.main-container{
+  background: rgb(230,247,255);
+}
 .infoContainerDiv {
-  padding: 5px;
 }
 
 .profilePic {
-  max-width: 90px;
+  border-radius: 50%;
+  max-height: 100px;
+  max-width: 100px;
 }
 
+.image-container{
+  border-radius: 50%;
+  height: 120px;
+  width: 120px;
+  background-color: rgba(88, 170, 199, 0.74);
+  margin-bottom: 10px;
+}
+.user-info{
+  display: table-row;
+}
+.user-info-wrapper{
+  border: solid rgba(1, 87, 103, 0.4);
+  background-color: rgba(70, 156, 175, 0.2);
+}
+.sign-out-container{
+  background-color: rgba(255, 0, 0, 0.9);
+  color: rgba(255, 255, 255, 0.9);
+}
 @media screen and (max-width: 992px) and (min-width: 768px) {
   .cardDiv {
     width: 50%;
