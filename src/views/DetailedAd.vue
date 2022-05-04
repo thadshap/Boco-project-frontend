dee4c5893069da033de294bcf35d0a5fdcef5064
Maiken Louise  Brechan <maikebre@stud.ntnu.com>
2022-05-04 14:25:53 +0200

    Initial commit for carousell

--- src/views/DetailedAd.vue
+++ src/views/DetailedAd.vue
@@ -7,7 +7,28 @@
       id="adPictureDiv"
       class="text-center"
     >
-      <img v-for="picture in pictures" :src="picture" :key="picture">
+
+
+    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <div class="images-wrapper">
+            <img v-for="picture in pictures" :src="picture" :key="picture">
+          </div>
+        </div>
+      </div>
+      <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
+    </div>
+      
+
+
     </div>
     <div class="text-center">
       <label class="form-label">
