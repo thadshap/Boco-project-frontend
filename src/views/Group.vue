6585cd9da9ab7cc7d4dd68ec808c1297a98a813a
leo <leonarss@stude.ntnu.no>
2022-05-04 09:47:05 +0200

    feat: make group by email

--- src/views/Group.vue
+++ src/views/Group.vue
@@ -1,6 +1,9 @@
 <template>
 <div class="d-flex flex-column screen">
   <div class="d-flex flex-column groups">
+      <div v-if="!groups.length">Ser ut som du har ingen chatter</div>
+      <div>Start en chat:</div>
+      <div><input type="text" placeholder="Epost" class="add"></div>
       <GroupComponent 
       v-for="group in groups"
       :key="group"
@@ -55,7 +58,6 @@ export default {
 .groups{
     width: 100vw;
 }
-
 /* Buttons */
 .menu-button{
     background: transparent;
