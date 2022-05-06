dad281e8e328b2e2897dbc888094f51a29e413b1
Anders <andetel@stud.ntnu.no>
2022-05-06 20:24:34 +0200

    Added javascript docs

--- src/services/adService.js
+++ src/services/adService.js
@@ -3,55 +3,13 @@ let url = 'http://localhost:'
 let port = "8443"
 
 export default {
-    /**
-     * Method to delete an ad
-     * @param adId is the id for the ad to be deleted
-     */
-    deleteAd(adId) {
-        const options = {
-            method: "DELETE",
-            url: `${url}${port}/api/auth/ads/` + adId,
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': 'Bearer ' + localStorage.getItem("token")
-            },
-        };
 
-        return axios.request(options);
-    },
     /**
-     * Method to update an ad, can change one to all attributes
+     * Gets an ad by id
+     *
+     * @param adId id for the ad to get
+     * @returns {Promise<AxiosResponse<any>>}
      */
-    updateAd(
-        adId,
-        newTitle,
-        newPrice,
-        newDescription,
-        newDurationType,
-        newCategoryId,
-        newStreetAdress,
-        newPostalCode
-    ) {
-        const options = {
-            method: "PUT",
-            url: `${url}${port}/api/auth/ads/` + adId,
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': 'Bearer ' + localStorage.getItem("token")
-            },
-            data: {
-                title: newTitle,
-                price: newPrice,
-                description: newDescription,
-                durationType: newDurationType,
-                categoryId: newCategoryId,
-                streetAddress: newStreetAdress,
-                postalCode: newPostalCode
-            },
-        };
-
-        return axios.request(options);
-    },
     getAdById(adId) {
         const options = {
             method: "GET",
@@ -64,24 +22,13 @@ export default {
 
         return axios.request(options);
     },
-    /**
-     * @param adId API call for adding picture have adId in the return, which is what you sends to add picture afterwards
-     * @param pictureLink is the link to the picture, example format: 'C:UsersKarolOneDriveDiversePicturesdayEivind_Hellstrom.jpg'
-     */
-    uploadPicturesForAd(adId, pictures){
-        let userId = localStorage.getItem("userId");
 
-        const options = {
-            method: 'POST',
-            url: `${url}${port}/api/auth/ads/newPicture/${userId}/${adId}`,
-            headers: {
-                'Content-Type': 'multipart/form-data; boundary=---011000010111000001101001',
-                'Authorization': 'Bearer ' + localStorage.getItem("token")
-            },
-            data: pictures
-        };
-        return axios.request(options);
-    },
+    /**
+     * Gets the first picture for an ad from the database
+     *
+     * @param adId id of the ad
+     * @returns {Promise<AxiosResponse<any>>}
+     */
     getPictureForAd(adId) {
         const options = {
             method: 'GET',
@@ -93,6 +40,13 @@ export default {
         };
         return axios.request(options);
     },
+
+    /**
+     * Gets all pictures for an ad
+     *
+     * @param adId id of the ad
+     * @returns {Promise<AxiosResponse<any>>}
+     */
     getPicturesForAd(adId) {
         const options = {
             method: 'GET',
@@ -104,6 +58,13 @@ export default {
         };
         return axios.request(options);
     },
+
+    /**
+     * Gets all unavailable dates for an ad
+     *
+     * @param adId id of the ad
+     * @returns {Promise<AxiosResponse<any>>}
+     */
     getAllUnavailableDatesForAd(adId){
         const options = {
             method: 'POST',
@@ -116,8 +77,9 @@ export default {
         };
         return axios.request(options);
     },
+
     /**
-     *Posts a new ad
+     *Posts a new ad without pictures
      *
      * @param title title of the ad
      * @param description description of the ad
@@ -156,6 +118,25 @@ export default {
             },
         };
 
+        return axios.request(options);
+    },
+
+    /**
+     * @param adId API call for adding picture have adId in the return, which is what you sends to add picture afterwards
+     * @param pictures {Array<Object>} of pictures to upload
+     */
+    uploadPicturesForAd(adId, pictures){
+        let userId = localStorage.getItem("userId");
+
+        const options = {
+            method: 'POST',
+            url: `${url}${port}/api/auth/ads/newPicture/${userId}/${adId}`,
+            headers: {
+                'Content-Type': 'multipart/form-data; boundary=---011000010111000001101001',
+                'Authorization': 'Bearer ' + localStorage.getItem("token")
+            },
+            data: pictures
+        };
         return axios.request(options);
     }
 };
