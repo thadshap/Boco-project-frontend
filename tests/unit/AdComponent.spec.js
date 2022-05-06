d24da331298d42277f0589e9154fae71837ee012
Eirin Svinsås <eirinsvi@stud.ntnu.no>
2022-05-06 22:10:04 +0200

    tests

--- tests/unit/AdComponent.spec.js
+++ tests/unit/AdComponent.spec.js
@@ -1,7 +1,33 @@
-import { shallowMount } from "@vue/test-utils";
+import {mount, shallowMount} from "@vue/test-utils";
 import AdComponent from "@/components/AdComponent";
-import store from "@/store"
+import {createStore} from "vuex";
+import DetailedAd from "@/views/DetailedAd";
 
+const store = createStore({
+    state:{
+        currentAd: {}
+    },
+    mutations: {
+        SET_CURRENT_AD(state, currentAd) {
+            state.currentAd = currentAd
+        },
+    },
+    actions: {
+        setCurrentAd({ commit }, currentAd) {
+            commit("SET_CURRENT_AD", currentAd)
+        },
+    },
+    getters:{
+        currentAd(state) {
+            return state.currentAd
+        }
+    }
+})
+
+jest.spyOn(AdComponent.methods, "goToDetailedView")
+    .mockImplementation(() => {
+        AdComponent.data().disable.push([new Date()])
+    })
 
 describe("AdComponent.vue", () => {
     it("renders props when passed", () => {
@@ -32,4 +58,36 @@ describe("AdComponent.vue", () => {
         });
         expect(wrapper.find("img").attributes("src")).toBe(`${image}`)
     });
+
+    it("test that clicking on 'Send melding' routes to detailed ad", async () => {
+
+        const id = 1;
+        const title = "Motorsag";
+        const price = 1000;
+        const place = "Trondheim";
+        const image = "BoCo.png";
+        const userId = 1
+
+        const mockRoute = {}
+
+        const mockRouter = {
+            push: jest.fn()
+        }
+
+        const wrapper = mount(AdComponent, {
+            props: { id,title,price,place,image,userId },
+            global: {
+                mocks: {
+                    $route: mockRoute,
+                    $router: mockRouter
+                }
+            }
+        })
+        let btn = wrapper.findAll('a').filter(a => a.text() === "Til annonse")[0]
+
+        await btn.trigger("click")
+
+        expect(mockRouter.push).toHaveBeenCalledTimes(1)
+        expect(mockRouter.push).toHaveBeenCalledWith({"name": "Ad"})
+    })
 });
