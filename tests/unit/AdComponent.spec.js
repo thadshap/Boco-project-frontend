482bc99c2b43742157695e27db22d5afee67d13e
Anders <andetel@stud.ntnu.no>
2022-05-06 14:24:03 +0200

    Added test to ad component

--- tests/unit/AdComponent.spec.js
+++ tests/unit/AdComponent.spec.js
@@ -5,7 +5,6 @@ import store from "@/store"
 
 describe("AdComponent.vue", () => {
     it("renders props when passed", () => {
-        expect(true).toBe(true)
         const id = 1;
         const title = "Motorsag";
         const price = 1000;
@@ -17,5 +16,19 @@ describe("AdComponent.vue", () => {
             props: { id,title,price,place,image,userId },
         });
         expect(wrapper.text()).toContain(`${title}${price} kr Trondheim Send melding  Til annonse`);
+    });
+
+    it("Renders image when passed", () => {
+        const id = 1;
+        const title = "Motorsag";
+        const price = 1000;
+        const place = "Trondheim";
+        const image = "BoCo.png";
+        const userId = 1
+        const wrapper = shallowMount(AdComponent, {
+            global:{ plugins: [store]},
+            props: { id,title,price,place,image,userId },
+        });
+        expect(wrapper.find("img").attributes("src")).toBe(`${image}`)
     });
 });
